c9e461fc132ade2831f89ec0cf517002c0761e9e
Volodymyr Baraban <barabanv2003@gmail.com>
2022-04-25 14:20:46 +0300

    2

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,16 @@
-"use strict";
 let ref = {
-    listEl: document.querySelector("#categories"),
-    itemEl: document.querySelectorAll(".item"),
-    };
+  listEl: document.querySelector('#categories'),
+  itemEl: document.querySelectorAll('.item'),
+};
 
 console.log(`Number of categories: ${ref.itemEl.length}
  `);
 
 ref.itemEl.forEach(e => {
-    console.log(`Category: ${e.firstElementChild.textContent} `);
-    console.log(`Elements: ${e.lastElementChild.childElementCount}
+  console.log(`Category: ${e.firstElementChild.textContent} `);
+  console.log(`Elements: ${e.lastElementChild.childElementCount}
      `);
 });
-
+const b = 4;
+const c = 3;
+const a = b || c ? b + c : b - c;
